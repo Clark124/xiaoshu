031bb34d882a166a3e0bb9cdd624dbce6b88f389
clark124 <53799906@qq.com>
2017-08-20 13:39:15 +0800

    prot

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose')
 // var db = mongoose.createConnection('mongodb://127.0.0.1:27017/studentsystem')
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/blog');
+mongoose.connect('mongodb://47.91.156.35/blog');
 var db = mongoose.connection;
 db.once('open', function (callback) {
     console.log('数据库成功连接');
