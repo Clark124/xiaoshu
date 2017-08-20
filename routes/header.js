ea3badac8213371babcd2d959023b4f5de7f9c50
clark124 <53799906@qq.com>
2017-08-20 13:03:54 +0800

    first commit

---
+++ routes/header.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user')
+
+router.post('/userInfo', function (req, res, next) {
+    let { username } = req.body
+    User.find({ username }, (error, result) => {
+        if (error) {
+            res.send({ status: 500, error: error });
+            return;//服务器错误 
+        }
+        const { username, email, userId, createTime, avatar, fans, attentions, favorite, sex, resume } = result[0]
+        const data = { username, email, userId, createTime, avatar, fans, favorite, attentions, sex, resume }
+        res.send({ status: 200, data: data })
+    })
+});
+
+module.exports = router;
