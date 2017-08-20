712c07d3900dfe2cc3d752b0b03fac3f3b664204
clark124 <53799906@qq.com>
2017-08-20 15:57:34 +0800

    change title

--- models/db.js
+++ models/db.js
@@ -10,52 +10,5 @@ db.once('open', function (callback) {
 })
 
 
-// var userScheme = new mongoose.Schema({
-//     "id": Number,
-//     "username": String,
-//     "password": String,
-//     "avatar": String,
-//     "collections": [{
-//         "collectionid": Number,
-//         "collectionName": String,
-//         "notebook": [{
-//             "noteid": Number,
-//             "title": String,
-//             "content": String,
-//             "praise": [String],
-//             "comment": [{ "username": String, "content": String, "dateTime": Date }]
-//         }]
-//     }]
-// })
-
-// var User = mongoose.model('user', userScheme)
-
-// const obj = {
-//     "id": 2,
-//     "username": 'Clark',
-//     "password":198701124,
-//     "avatar": 'mr.jpg',
-//     "collections": [{
-//         "collectionid": 1000,
-//         "collectionName": "日记本",
-//         "notebook": [{
-//             "noteid": 10001,
-//             "title": 'javascript',
-//             "content": '好好学习',
-//             "praise": ["cheche"],
-//             "comment": [{
-//                 "username": "cheche",
-//                 "content": "非常好"
-//             }]
-//         }
-//       ]
-//     }
-
-//     ]
-// }
-// User.create(obj,(error)=>{
-//     console.log('保存成功')
-// })
-
 
 module.exports = db;
