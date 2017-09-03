cc84f1807ae12b4db977f9c57ed6d9ad92146c8b
clark124 <53799906@qq.com>
2017-09-03 10:42:21 +0800

    add articlelen

--- routes/header.js
+++ routes/header.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user')
-
+var Article = require('../models/article')
 router.post('/userInfo', function (req, res, next) {
     let { username } = req.body
     User.find({ username }, (error, result) => {
@@ -9,14 +9,24 @@ router.post('/userInfo', function (req, res, next) {
             res.send({ status: 500, error: error });
             return;//服务器错误 
         }
-        const { username, email, userId, createTime, avatar, fans, attentions, favorite, sex, resume } = result[0]
-        const data = { username, email, userId, createTime, avatar, fans, favorite, attentions, sex, resume }
-        res.send({ status: 200, data: data })
+        Article.find({username},(error,articles)=>{
+            if (error) {
+                res.send({ status: 500, error: error });
+                return;//服务器错误 
+            }
+            const articlelen = articles.length
+            const { username, email, userId, createTime, avatar, fans, attentions, favorite, sex, resume } = result[0]
+            const data = { username, email, userId, createTime, avatar, fans, favorite, attentions, sex, resume,articlelen }
+            res.send({ status: 200, data: data })
+        })
+        
     })
 });
 // router.get('/getSession',function(req,res){
-//     console.log(req.session.user)
-    
-//     res.send({status:404})
+//     if(req.session.username){
+//         res.send({status:200,data:req.session.username})
+//     }else{
+//         req.send({status:404})
+//     }
 // })
 module.exports = router;
